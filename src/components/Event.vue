b0d64d16dfe14787d7bbbaec05c052ef10b0e729
Alfonso Guevara <aguevara1@levi.com>
2021-04-10 13:47:16 -0700

    adding Event

---
+++ src/components/Event.vue
@@ -0,0 +1,47 @@
+<template>
+  <registration v-if="!registered" ></registration>
+  <shoppable-video v-else></shoppable-video>
+</template>
+
+<script>
+import Registration from "../components/registration/Registration.vue";
+import ShoppableVideo from "./ShoppableVideo";
+
+import { computed} from "vue";
+import userState from "@/state/userState"
+
+export default {
+  setup() {
+
+    // const registered = ref(false);
+    const {state} = userState();
+
+    const registered = computed(() => {
+      console.log("STATE :: ", state.value);
+      return state.value.registered;
+    });
+
+    return {
+      registered
+    };
+  },
+  
+  components: {
+    Registration,
+    ShoppableVideo
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../assets/scss/_variables.scss";
+@import "../assets/scss/lscoicons.scss";
+@import "../assets/scss/levi-fonts.scss";
+
+h2 {
+    background: white;
+    font-weight: 900;
+    line-height: 36px;
+    font-family: arial, sans-serif;
+}
+</style>
